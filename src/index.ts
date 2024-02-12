0ac647acf02cf9d3422ce633ad6ebd945337879a
zweroboy1 <fprognoz@gmail.com>
2024-02-12 17:35:44 +0200

    feat: add 404 error for invalid paths

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,20 @@ dotenv.config({ path: '.env' });
 
 const PORT = process.env.PORT || 4000;
 
-const server = http.createServer((_request, response) => {
-  response.write('Server works!\n');
-  response.write(Date.now().toString());
-  response.end();
+const server = http.createServer((request, response) => {
+  const path = request.url || '';
+
+  if (path === '/api/users') {
+    response.write('API endpoint works!\n');
+    response.write(Date.now().toString());
+    response.end();
+  } else {
+    response.writeHead(404, { 'Content-Type': 'text/plain' });
+    response.write('404 Not Found');
+    response.end();
+  }
 });
+
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
